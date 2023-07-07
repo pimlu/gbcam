b779802243b64aac50816a9ac544ef704e6bd5da
Stuart Geipel <lapimlu@gmail.com>
2023-07-07 19:36:07 -0400

    build: quality of life improvements

---
+++ .config.sh
@@ -0,0 +1,9 @@
+
+# set these yourself
+
+# https://wellys.com/posts/rp2040_c_macos/
+
+# clone of pico sdk
+export PICO_SDK_PATH=/path/to/pico/pico-sdk
+# official arm toolchain
+export PICO_TOOLCHAIN_PATH=/Applications/ARM/bin
--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# set this yourself
-export PICO_SDK_PATH=/Users/stuart/Documents/builds/pico/pico-sdk
-export PICO_TOOLCHAIN_PATH=/Applications/ARM/bin
+export GBCAM_PATH="$(cd "$(dirname "$0")" && pwd)"
+
+if ! [ -f "$GBCAM_PATH/.config.local.sh" ]; then
+    echo "Error: Must create .config.local.sh based off .config.sh" 1>&2
+    return 1
+fi
+
+source "$GBCAM_PATH/.config.local.sh"
 
 (
     set -e
-    cd "$(dirname "$0")"
+    cd "$GBCAM_PATH"
 
     rm -rf build/
     mkdir build
