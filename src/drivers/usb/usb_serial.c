3874677bcf5b73ff1f0683bed3cff5341dca7932
Stuart Geipel <lapimlu@gmail.com>
2023-07-08 13:46:30 -0400

    usb: get serial with handrolled tinyusb working

---
+++ src/drivers/usb/usb_serial.c
@@ -0,0 +1,12 @@
+#include "usb_serial.h"
+
+#include "pico/stdlib.h"
+#include "tusb.h"
+
+// idk why this is necessary, but it seems sleeping by itself is not enough
+void wait_for_serial_hack() {
+    for (int i=0; i<500; i++) {
+        tud_task();
+        sleep_ms(1);
+    }
+}
--- src/main.c
+++ src/main.c
@@ -11,12 +11,29 @@
 #include "hardware/adc.h"
 #include "pico/multicore.h"
 
+#include "bsp/board.h"
+#include "tusb.h"
+
 #include "drivers/gbcam.h"
 #include "drivers/sharp.h"
+#include "drivers/usb/usb_serial.h"
 
 #include "utils.h"
 
 #define LED 13
+
+
+enum  {
+  BLINK_NEVER_MOUNTED = 250,
+  BLINK_MOUNTED = 1000,
+  BLINK_SUSPENDED = 2500,
+  BLINK_UNPLUGGED = 100,
+};
+
+static uint32_t blink_interval_ms = BLINK_NEVER_MOUNTED;
+
+
+void led_blinking_task(void);
 
 
 // 16KB, kind of brutal but we have way more than that
@@ -37,9 +54,10 @@ void camera_snap() {
 }
 
 int main() {
-    stdio_init_all();
-    
-    adc_init();
+    board_init();
+    tusb_init();
+    stdio_usb_init();
+    tud_task();
 
     gpio_init(LED);
     gpio_set_dir(LED, GPIO_OUT);
@@ -58,17 +76,72 @@ int main() {
     //     vcom = !vcom;
     // }
 
-    // to wait for serial to come up
-    sleep_ms(1000);
-    printf("hello\n");
-
-    // snap the camera on core1 since core0 is busy with USB/serial interrupts
-    multicore_launch_core1(camera_snap);
+    // wait for USB to come up
+    while(!tud_ready()) {
+        tud_task();
+    }
+    wait_for_serial_hack();
+    
+    printf("start\n");
 
     // can't let main return otherwise it breaks core1
     while (true) {
-        sleep_ms(1000);
+        led_blinking_task();
+
+        tud_task(); // tinyusb device task
     }
 
     return 0;
+}
+
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void)
+{
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void)
+{
+  blink_interval_ms = BLINK_UNPLUGGED;
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+  (void) remote_wakeup_en;
+  blink_interval_ms = BLINK_SUSPENDED;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void)
+{
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+
+//--------------------------------------------------------------------+
+// BLINKING TASK
+//--------------------------------------------------------------------+
+void led_blinking_task(void)
+{
+  static uint32_t start_ms = 0;
+  static bool led_state = false;
+
+  // Blink every interval ms
+  if ( board_millis() - start_ms < blink_interval_ms) return; // not enough time
+  start_ms += blink_interval_ms;
+
+
+  printf("writing %d\n", led_state);
+
+  gpio_put(LED, led_state);
+  led_state = 1 - led_state; // toggle
 }
